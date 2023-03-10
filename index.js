e3cac24374b270be5cb4f8b5cc3f4c7de0c5f0af
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-03-10 13:10:56 +0200

    Test server 4

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client/build'));
+app.use(express.static('./client/build'));
 
 // // app.post('/api/registration', (req, res) => {
 // //   try {
