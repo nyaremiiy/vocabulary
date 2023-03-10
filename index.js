04b27302f46133d16f7afd5ecd77af6f73c130b5
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-03-10 12:29:32 +0200

    Testing the get request 3

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
-const express = require('express');
-const config = require('config');
-const cors = require('cors');
-const log = console.log;
+// const express = require('express');
+// const config = require('config');
+// const cors = require('cors');
+// const log = console.log;
 
+// const app = express();
+
+// const PORT = config.get('port');
+
+// app.use(cors());
+// app.use(express.json());
+// // app.use(express.static('client/build'));
+
+// // app.post('/api/registration', (req, res) => {
+// //   try {
+// //     log(req.body);
+// //     const { email, password } = req.body;
+
+// //     res.status(201).json({
+// //       email,
+// //       password,
+// //       token: '123456',
+// //     });
+// //   } catch (error) {
+// //     console.log('Error: ', error);
+// //   }
+// // });
+
+// // app.get('/', (_, res) => {
+// //   res.sendFile(path.join(__dirname, 'client/build/index.html'));
+// // });
+// app.get('/', (req, res) => {
+//   res.send('Express on Vercel');
+// });
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log(`The server is running on the port: ${PORT}`);
+// });
+
+// Add Express
+const express = require('express');
+
+// Initialize Express
 const app = express();
 
-const PORT = config.get('port');
-
-app.use(cors());
-app.use(express.json());
-// app.use(express.static('client/build'));
-
-// app.post('/api/registration', (req, res) => {
-//   try {
-//     log(req.body);
-//     const { email, password } = req.body;
-
-//     res.status(201).json({
-//       email,
-//       password,
-//       token: '123456',
-//     });
-//   } catch (error) {
-//     console.log('Error: ', error);
-//   }
-// });
-
-// app.get('/', (_, res) => {
-//   res.sendFile(path.join(__dirname, 'client/build/index.html'));
-// });
+// Create GET request
 app.get('/', (req, res) => {
   res.send('Express on Vercel');
 });
 
-app.listen(PORT, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(`The server is running on the port: ${PORT}`);
+// Initialize server
+app.listen(5000, () => {
+  console.log('Running on port 5000.');
 });
