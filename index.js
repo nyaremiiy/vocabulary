afbc40583c9f726610cef4a3993082607d116c7e
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-03-10 15:36:58 +0200

    Connected config

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// const express = require('express');
-// const path = require('node:path');
-// // const config = require('config');
-// const cors = require('cors');
 import express from 'express';
 import path from 'node:path';
 import cors from 'cors';
+import config from 'config';
+
+
+
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +13,11 @@ const __dirname = path.dirname(__filename);
 const log = console.log;
 
 const app = express();
+
+log('Port:', config.get('port'))
+
+
+const PORT = process.env.PORT|| config.get('port') || 5000;
 
 
 app.use(cors());
@@ -38,9 +44,9 @@ app.get('/', function (_, res) {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log(`The server is running on the port: ${5000}`);
+  console.log(`The server is running on the port: ${PORT}`);
 });
